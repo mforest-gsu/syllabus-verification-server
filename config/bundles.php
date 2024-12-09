f738e1ff4e98e6e3c7a767c834efa8c985916191
Melody Kimball <mforest@gsu.edu>
2024-12-09 11:42:03 -0500

    Added Doctrine ORM and Symfony Maker bundles

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Gadget\Log\GadgetLogBundle::class => ['all' => true],
     Gadget\Console\GadgetConsoleBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
