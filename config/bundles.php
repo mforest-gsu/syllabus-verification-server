1a92d0700bd63d0a26c46f64d825888a96d25e2a
Melody Kimball <mforest@gsu.edu>
2024-12-10 08:55:40 -0500

    Added mjfk23/gadget-http and firebase/php-jwt

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Gadget\Cache\GadgetCacheBundle::class => ['all' => true],
+    Gadget\Http\GadgetHttpBundle::class => ['all' => true],
 ];
